77bd340e7de1c421976dcd3c5c50a717abd02aa6
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-07-25 10:25:47 -0300

    refactor: change card routes for 'cards'

--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -1,11 +1,18 @@
-import { Router } from 'express';
-import { createCard, getCard, updateCard, deleteCard } from '../controllers/cardController.js';
+import { Router } from "express";
+import {
+  createCard,
+  getCard,
+  updateCard,
+  deleteCard,
+  playCard,
+} from "../controllers/cardController.js";
 
 const router = Router();
 
-router.post('/card', createCard);
-router.get('/card/:id', getCard);
-router.put('/card/:id', updateCard);
-router.delete('/card/:id', deleteCard);
+router.post("/cards", createCard);
+router.post("/cards/play", playCard);
+router.get("/cards/:id", getCard);
+router.put("/cards/:id", updateCard);
+router.delete("/cards/:id", deleteCard);
 
 export default router;
