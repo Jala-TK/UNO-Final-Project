62a8d90b1a7016173cf9f2787e68aa35a9d35b84
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-07-25 10:18:19 -0300

    style: apply linter

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: 'An error occurred:', error: err.message });
+  res.status(500).json({ message: "An error occurred:", error: err.message });
 };
 
 export default errorHandler;
