b5f0be0d3ec7478634120227f5ff1103a0bb737c
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-07-25 10:52:53 -0300

    fix: solving error of creating even when finding existing user

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,15 +1,24 @@
 import { createHash } from "crypto";
 import Player from "../models/player.js";
+import { Op } from "sequelize";
 
 export const createPlayer = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
 
     let user = await Player.findOne({
-      where: { username: username },
+      where: {
+        [Op.or]: [{ username: username }, { email: email }],
+      },
     });
 
-    if (user) res.status(400).json({ error: "User already exists" });
+    if (user) {
+      if (user.username === username) {
+        return res.status(400).json({ error: "User already exists" });
+      } else if (user.email === email) {
+        return res.status(400).json({ error: "Email already exists" });
+      }
+    }
 
     const hashedPassword = createHash("sha256")
       .update(password)
