d7cce23b1d6fd52dcac94a052a28edf4d950f380
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-07-25 10:28:31 -0300

    style: apply linter

--- src/routes/scoreRoutes.js
+++ src/routes/scoreRoutes.js
@@ -1,11 +1,18 @@
-import { Router } from 'express';
-import { createScore, getScore, updateScore, deleteScore } from '../controllers/scoreController.js';
+import { Router } from "express";
+import {
+  createScore,
+  getScore,
+  updateScore,
+  deleteScore,
+  getPlayerScores,
+} from "../controllers/scoreController.js";
 
 const router = Router();
 
-router.post('/score', createScore);
-router.get('/score/:id', getScore);
-router.put('/score/:id', updateScore);
-router.delete('/score/:id', deleteScore);
+router.post("/score", createScore);
+router.get("/score/:id", getScore);
+router.get("/score", getPlayerScores);
+router.put("/score/:id", updateScore);
+router.delete("/score/:id", deleteScore);
 
 export default router;
