6c6542673cda375e7ebaf08ec0d142fedec3c302
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-07-25 10:27:33 -0300

    style: apply linter

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,20 +1,35 @@
-import { Router } from 'express';
-import { createGame, getGame, getStatusGame, getPlayersInGame, getCurrentPlayer, updateGame, deleteGame, joinGame, getReady, leaveGame, startGame } from '../controllers/gameController.js';
+import { Router } from "express";
+import {
+  createGame,
+  getGame,
+  getStatusGame,
+  getPlayersInGame,
+  getCurrentPlayer,
+  updateGame,
+  deleteGame,
+  joinGame,
+  getReady,
+  leaveGame,
+  startGame,
+  getTopCard,
+  getPlayerHands,
+} from "../controllers/gameController.js";
 
 const router = Router();
 
-router.post('/games', createGame);
-router.get('/games/:id', getGame);
-router.put('/games/:id', updateGame);
-router.delete('/games/:id', deleteGame);
+router.post("/games", createGame);
+router.get("/games/:id", getGame);
+router.put("/games/:id", updateGame);
+router.delete("/games/:id", deleteGame);
 
-
-router.post('/game/join', joinGame);
-router.post('/game/ready', getReady);
-router.post('/game/leave', leaveGame);
-router.post('/game/start', startGame);
-router.get('/game/status', getStatusGame);
-router.get('/game/players', getPlayersInGame);
-router.get('/game/currentPlayer', getCurrentPlayer);
+router.post("/game/join", joinGame);
+router.post("/game/ready", getReady);
+router.post("/game/leave", leaveGame);
+router.post("/game/start", startGame);
+router.get("/game/status", getStatusGame);
+router.get("/game/players", getPlayersInGame);
+router.get("/game/currentPlayer", getCurrentPlayer);
+router.get("/game/topCard", getTopCard);
+router.get("/game/playerHands", getPlayerHands);
 
 export default router;
