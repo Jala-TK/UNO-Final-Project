d07677de4e1f54b3c2d699a05223b8149950022f
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-07-31 02:15:01 -0300

    refactor: add more checkers

--- src/routes/player/deletePlayer.js
+++ src/routes/player/deletePlayer.js
@@ -1,11 +1,25 @@
 import Player from '../../models/player.js';
+import VerifyToken from '../../utils/verifyToken.js';
 
 export const deletePlayer = async (req, res, next) => {
   try {
-    const player = await Player.findByPk(req.params.id);
+    const id = req.params.id;
+    const player = await Player.findByPk(id);
+
     if (!player || player?.auditExcluded) {
       return res.status(404).json({ message: 'Player not found' });
     }
+
+    const { access_token } = req.body;
+    if (!access_token) {
+      return res.status(400).json({ error: 'Invalid params' });
+    }
+
+    const user = await VerifyToken(access_token);
+    if (user?.id != id) {
+      return res.status(401).json({ error: 'Unauthorized delete this user' });
+    }
+
     await player.update({ auditExcluded: true });
     res.status(204).send();
   } catch (error) {
--- src/routes/player/updatePlayer.js
+++ src/routes/player/updatePlayer.js
@@ -1,18 +1,38 @@
 import Player from '../../models/player.js';
+import VerifyToken from '../../utils/verifyToken.js';
+import { Op } from 'sequelize';
 
 export const updatePlayer = async (req, res, next) => {
   try {
-    const player = await Player.findByPk(req.params.id);
+    const id = req.params.id;
+    const player = await Player.findByPk(id);
     if (!player || player?.auditExcluded) {
       return res.status(404).json({ message: 'Player not found' });
     }
-    const { username, email, password } = req.body;
+    const { username, email, password, access_token } = req.body;
 
-    if (!username || !email || !password)
+    if (!username || !email || !password || !access_token) {
       return res.status(400).json({ error: 'Invalid params' });
+    }
+
+    const user = await VerifyToken(access_token);
+    if (user?.id != id) {
+      return res.status(401).json({ error: 'Unauthorized update this user' });
+    }
+
+    const existingPlayer = await Player.findOne({
+      where: {
+        [Op.or]: [{ username: username }, { email: email }],
+      },
+    });
+    if (existingPlayer && existingPlayer?.id !== user.id) {
+      return res
+        .status(400)
+        .json({ message: 'There is already a player with this data' });
+    }
 
     await player.update({ username, email, password });
-    res.status(200).json(player);
+    res.status(200).json({ message: 'User updated sucessfully' });
   } catch (error) {
     next(error);
   }
