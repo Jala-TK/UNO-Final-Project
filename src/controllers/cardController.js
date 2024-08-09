267838234992c201857899195b0969f19fef8a2b
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-08-09 16:55:42 -0300

    refactor: update controller to use middleware of authentication token

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -1,18 +1,19 @@
+import { Router } from 'express';
 import { createCard } from '../routes/card/createCard.js';
 import { getCard } from '../routes/card/getCard.js';
 import { updateCard } from '../routes/card/updateCard.js';
 import { deleteCard } from '../routes/card/deleteCard.js';
 import { playCard } from '../routes/card/playCard.js';
 import { drawCard } from '../routes/card/drawCard.js';
-import { Router } from "express";
+import { authMiddleware } from '../middleware/authenticate.js';
 
 const router = Router();
 
-router.post("/cards", createCard);
-router.post("/cards/play", playCard);
-router.post("/cards/draw", drawCard);
-router.get("/cards/:id", getCard);
-router.put("/cards/:id", updateCard);
-router.delete("/cards/:id", deleteCard);
+router.post('/cards', createCard);
+router.post('/cards/play', authMiddleware, playCard);
+router.post('/cards/draw', authMiddleware, drawCard);
+router.get('/cards/:id', getCard);
+router.put('/cards/:id', updateCard);
+router.delete('/cards/:id', deleteCard);
 
 export default router;
--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -3,12 +3,13 @@ import { createPlayer } from '../routes/player/createPlayer.js';
 import { getPlayer } from '../routes/player/getPlayer.js';
 import { updatePlayer } from '../routes/player/updatePlayer.js';
 import { deletePlayer } from '../routes/player/deletePlayer.js';
+import { authMiddleware } from '../middleware/authenticate.js';
 
 const router = Router();
 
 router.post('/player', createPlayer);
 router.get('/player/:id', getPlayer);
-router.put('/player/:id', updatePlayer);
-router.delete('/player/:id', deletePlayer);
+router.put('/player/:id', authMiddleware, updatePlayer);
+router.delete('/player/:id', authMiddleware, deletePlayer);
 
 export default router;
