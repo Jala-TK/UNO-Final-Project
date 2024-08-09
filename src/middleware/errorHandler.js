5d4467abe1742c97299cb503efa259ef22dccb07
Kainan Rodrigues <kainan.ars@hotmail.com>
2024-08-09 16:55:01 -0300

    feat: add logger to error handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,18 @@
+import logger from '../utils/logger.js';
+
 const errorHandler = (err, req, res, next) => {
+  logger.error({
+    message: err.message,
+    stack: err.stack,
+    path: req.path,
+    method: req.method,
+    body: req.body,
+  });
+
   const statusCode = err.code || 500;
   if (statusCode == 500) console.error(err.stack);
   const message = err.message || {
-    message: "An error occurred:",
+    message: 'An error occurred:',
   };
 
   res.status(statusCode).json({
