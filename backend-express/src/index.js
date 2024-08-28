ad20bfa6ac9e1199e2302a65410e6fbc395a081b
Kainan Rodrigues <kainan.ars@gmail.com>
2024-08-28 00:42:22 -0300

    feat: add socket.io server to front and back

--- backend-express/src/index.js
+++ backend-express/src/index.js
@@ -1,27 +1,44 @@
 import express from 'express';
+import http from 'http';
+import 'dotenv/config';
+import { Server as SocketIOServer } from 'socket.io';
 import sequelize from './config/database.js';
 import routes from './routes/index.js';
 import errorHandler from './middleware/errorHandler.js';
 import CacheMiddleware from './middleware/cacheMiddleware.js';
 import trackingMiddleware from './middleware/trackingMiddleware.js';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-const cacheOptions = {
-  max: 50,
-  maxAge: 2000,
-};
-app.use(trackingMiddleware);
-const cacheMiddleware = new CacheMiddleware(cacheOptions);
-app.use(cacheMiddleware.handleCache.bind(cacheMiddleware));
-
-app.use(errorHandler);
-
-app.use(express.json());
-app.use('/api', routes);
-
 const startServer = async () => {
+  const PORTHTTP = process.env.PORT || 3000;
+  const app = express();
+  const server = http.createServer(app);
+
+  const io = new SocketIOServer(server);
+
+  const cacheOptions = {
+    max: 50,
+    maxAge: 2000,
+  };
+  const cacheMiddleware = new CacheMiddleware(cacheOptions);
+
+  app.use(trackingMiddleware);
+  app.use(cacheMiddleware.handleCache.bind(cacheMiddleware));
+  app.use(express.json());
+  app.use(errorHandler);
+  app.use('/api', routes);
+
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+      console.log('Cliente desconectado');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+  });
+
   try {
     await sequelize.authenticate();
     console.log('\x1b[32m%s\x1b[0m', 'Conectado ao banco com sucesso.');
@@ -29,20 +46,21 @@ const startServer = async () => {
     await sequelize.sync();
     console.log('\x1b[32m%s\x1b[0m', 'Database sincronizado');
 
-    app.listen(PORT, () => {
-      console.log('\x1b[32m%s\x1b[0m', `Server rodando em localhost:${PORT}`);
+    server.listen(PORTHTTP, (err) => {
+      if (err) throw err;
+      console.log(`Server rodando em http://localhost:${PORTHTTP}`);
     });
   } catch (error) {
     console.error(
       '\x1b[31m%s\x1b[0m',
-      'Não é possível conectar ao banco dados:',
+      'Não é possível conectar ao banco de dados:',
       error,
     );
   }
+
+  return io;
 };
 
-if (process.env.NODE_ENV !== 'test') {
-  startServer();
-}
+const io = startServer();
 
-export default app;
+export { io };
