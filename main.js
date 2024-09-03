0b541845f8d85228d548e1508f457fbb7caee5ec
Tauã Ferreira <taua.doespirito@jala.university>
2024-09-03 00:07:12 -0300

    fix: error to build electron

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from 'electron';
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
   });
 
-  win.loadURL('http://localhost:80');
+  win.loadURL('http://localhost:80').catch((error) => {
+    console.error('Failed to load URL:', error);
+    setTimeout(loadURL, 2000);
+  });
+  win.setMenuBarVisibility(false);
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
   }
 });
